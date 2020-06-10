7059bedf64a20e6dd9331cecab606a8b33c8d445
Дмитрий Самсонов <NomekopSD@yandex.ru>
2020-06-10 19:00:32 +0300

    Base Entities and Message

---
+++ Koshelek.TestTask/Common/Koshelek.TestTask.Domain/Entities/Messages.cs
@@ -0,0 +1,12 @@
+﻿using Koshelek.TestTask.Domain.Entities.Base;
+using System;
+
+namespace Koshelek.TestTask.Domain.Entities
+{
+    public class Messages : BaseEntity
+    {
+        public string Text { get; set; }
+
+        public DateTime ServerDateTime { get; set; }
+    }
+}
