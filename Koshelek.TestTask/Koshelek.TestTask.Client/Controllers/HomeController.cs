a270fd264afbdc6fcd58cbf88a44a42f09d6223e
Дмитрий Самсонов <NomekopSD@yandex.ru>
2020-06-10 18:40:58 +0300

    First

---
+++ Koshelek.TestTask/Koshelek.TestTask.Client/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Koshelek.TestTask.Client.Models;
+
+namespace Koshelek.TestTask.Client.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult Sender()
+        {
+            return View();
+        }
+
+        public IActionResult Recipient()
+        {
+            return View();
+        }
+
+        public IActionResult Journal()
+        {
+            return View();
+        }
+    }
+}
---
+++ Koshelek.TestTask/Koshelek.TestTask.Client/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Koshelek.TestTask.Client.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
