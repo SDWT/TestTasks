5f1a35291e9e14709997562b2ae4d2a6691d61e1
Дмитрий Самсонов <NomekopSD@yandex.ru>
2020-06-10 18:52:19 +0300

    Transfer

---
+++ Koshelek.TestTask/Services/Koshelek.TestTask.API/Controllers/MessageController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Koshelek.TestTask.API.Controllers
+{
+    [Route("api/Messages")]
+    [ApiController]
+    public class MessageController : Controller
+    {
+        [HttpGet("{Id}"), ActionName("Get")]
+        public Message GetById(int Id)
+        {
+            return "WIP";
+        }
+
+    }
+}
