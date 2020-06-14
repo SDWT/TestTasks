54264960bee36fb66fddfec9ab4736a2b3bd1ca6
Дмитрий Самсонов <NomekopSD@yandex.ru>
2020-06-14 21:38:56 +0300

    delete useless actions in home controller Add comments

--- dc_Koshelek.TestTask/Koshelek.TestTask/Controllers/HomeController.cs
+++ dc_Koshelek.TestTask/Koshelek.TestTask/Controllers/HomeController.cs
@@ -21,25 +21,6 @@ namespace Koshelek.TestTask.Controllers
             return RedirectToAction("Sender");
         }
 
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
-        // public IActionResult Error()
-        // {
-        //     return View();
-        // }
-
         public IActionResult Sender()
         {
             _logger.LogInformation("Open Sender browser client");
--- dc_Koshelek.TestTask/Koshelek.TestTask/Controllers/MessageApiController.cs
+++ dc_Koshelek.TestTask/Koshelek.TestTask/Controllers/MessageApiController.cs
@@ -11,6 +11,9 @@ using Microsoft.Extensions.Logging;
 
 namespace Koshelek.TestTask.Controllers
 {
+    /// <summary>
+    /// Message api controller
+    /// </summary>
     public class MessageApiController : Controller
     {
         /// <summary>Messages data provider</summary>
@@ -18,9 +21,7 @@ namespace Koshelek.TestTask.Controllers
         private readonly IHubContext<MessagesHub> _HubContext;
         private readonly ILogger<MessageApiController> _logger;
 
-        /// <summary>
-        /// Messaage controller constroctor
-        /// </summary>
+        /// <summary>Messaage controller constroctor</summary>
         /// <param name="MessageData">Messages data provider</param>
         public MessageApiController(IMessageData MessageData, IHubContext<MessagesHub> HubContext, ILogger<MessageApiController> logger)
         {
@@ -29,12 +30,16 @@ namespace Koshelek.TestTask.Controllers
             _logger = logger;
         }
 
+        /// <summary>Post message to recipient (second) clients and database</summary>
+        /// <param name="connectionId">Client connection id</param>
+        /// <param name="message">Client message</param>
         [HttpPost, ActionName("Post")]
         public async Task Post(string connectionId, Message message)
         {
             message.ServerDateTime = DateTime.Now;
             _logger.LogDebug($"{message.ServerDateTime}| Receive message {message.Text}, Order: {message.Order}");
 
+            // send message to recipients
             await _HubContext.Clients.AllExcept(connectionId).SendAsync("ReceiveMessage", new
             {
                 message.Text,
@@ -43,14 +48,21 @@ namespace Koshelek.TestTask.Controllers
                 ServerDateTime = message.ServerDateTime.ToString("yyyy-MM-dd HH:mm:ss")
             });
 
+            // send message to database
             _MessageData.PostMessage(message);
         }
 
+        /// <summary>
+        /// Send message to database
+        /// </summary>
+        /// <param name="message">Message</param>
         public void PostMessage(Message message)
         {
             _MessageData.PostMessage(message);
         }
 
+        /// <summary>Get messages from database for last 1 minute</summary>
+        /// <param name="connectionId">Client connection id</param>
         [HttpPost]
         public async Task GetLast1MinMessages(string connectionId)
         {
@@ -69,6 +81,8 @@ namespace Koshelek.TestTask.Controllers
             await _HubContext.Clients.Client(connectionId).SendAsync("Receive", messages2);
         }
 
+        /// <summary>Get messages from database for last 10 minute</summary>
+        /// <param name="connectionId">Client connection id</param>
         public async Task GetLast10MinMessages(string connectionId)
         {
             DateTime Start, End = DateTime.Now;
@@ -85,7 +99,14 @@ namespace Koshelek.TestTask.Controllers
             }).ToArray();
             await _HubContext.Clients.Client(connectionId).SendAsync("Receive", messages2);
         }
-
+        /// <summary></summary>
+        /// <param name="connectionId">Client connection id</param>
+        /// <summary>
+        /// Get messages from database in period
+        /// </summary>
+        /// <param name="Start">Begin of period</param>
+        /// <param name="End">End of peri</param>
+        /// <returns></returns>
         public List<Message> GetMessagesByDate(DateTime Start, DateTime End = default(DateTime))
         {
             return _MessageData.GetMessagesByDate(Start, End);
